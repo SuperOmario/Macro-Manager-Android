0a64de9e13db0dcc11ffcaa430bb44503890bd71
SuperOmario <omarsallamala@gmail.com>
2022-02-26 22:11:42 +0000

    cleaned up UI a bit, food can now be edited from app,individual food and recipe details can be viewed, diary entries are grouped by date i.e all macros per day are shown in a singular view

---
+++ app/src/main/java/com/macro_manager/macroapp/IngredientDetails.java
@@ -0,0 +1,14 @@
+package com.macro_manager.macroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngredientDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ingredient_details);
+    }
+}
---
+++ app/src/main/java/com/macro_manager/macroapp/foodDetails.java
@@ -0,0 +1,14 @@
+package com.macro_manager.macroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class foodDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_details);
+    }
+}
